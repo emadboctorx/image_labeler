d8fe8bef47dad500e008420f6f97b5683925cc50
Emad <emad_1989@hotmail.com>
2020-03-02 11:40:35 -0800

    Fixed the images not fitting the window by creating 2 new classes

--- image_labeler.py
+++ image_labeler.py
@@ -1,16 +1,36 @@
-from PyQt5.QtGui import QIcon, QPixmap, QImage, QPainter, QPen
+from PyQt5.QtGui import QIcon, QPixmap, QPainter, QPen
 from PyQt5.QtWidgets import (QMainWindow, QApplication, QDesktopWidget, QAction, QStatusBar, QHBoxLayout,
-                             QVBoxLayout, QWidget, QLabel, QListWidget, QFileDialog)
+                             QVBoxLayout, QWidget, QLabel, QListWidget, QFileDialog, QFrame)
 from PyQt5.QtCore import Qt, QPoint, QRect
 from settings import *
 import sys
-import cv2
 import os
 
 
-class ImageArea(QLabel):
-    def __init__(self, main_window=None):
+class RegularImageArea(QLabel):
+    def __init__(self, current_image, main_window):
         super().__init__()
+        self.setFrameStyle(QFrame.StyledPanel)
+        self.current_image = current_image
+        self.main_window = main_window
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        current_size = self.size()
+        origin = QPoint(0, 0)
+        if self.current_image:
+            scaled_image = QPixmap(self.current_image).scaled(
+                current_size, Qt.IgnoreAspectRatio, Qt.SmoothTransformation)
+            painter.drawPixmap(origin, scaled_image)
+
+    def switch_image(self, img):
+        self.current_image = img
+        self.repaint()
+
+
+class ImageEditorArea(RegularImageArea):
+    def __init__(self, current_image, main_window):
+        super().__init__(current_image, main_window)
         self.main_window = main_window
         self.start_point = QPoint()
         self.end_point = QPoint()
@@ -18,7 +38,7 @@ class ImageArea(QLabel):
         self.end = QPoint()
 
     def paintEvent(self, event):
-        super().paintEvent(event)
+        RegularImageArea.paintEvent(self, event)
         qp = QPainter(self)
         pen = QPen(Qt.red)
         qp.setPen(pen)
@@ -50,7 +70,7 @@ class ImageArea(QLabel):
 
 class ImageLabelerBase(QMainWindow):
     def __init__(self, left_ratio, right_ratio, image_display_size, window_title='Image Labeler',
-                 current_image_area=QLabel):
+                 current_image_area=RegularImageArea):
         super().__init__()
         self.left_ratio = left_ratio
         self.right_ratio = right_ratio
@@ -59,9 +79,6 @@ class ImageLabelerBase(QMainWindow):
         self.current_image_area = current_image_area
         self.image_paths = {}
         self.setGeometry(50, 50, 1000, 600)
-        self.setWindowFlags(Qt.CustomizeWindowHint |
-                            Qt.WindowCloseButtonHint |
-                            Qt.WindowMinimizeButtonHint)
         self.window_title = window_title
         self.setWindowTitle(self.window_title)
         win_rectangle = self.frameGeometry()
@@ -71,7 +88,7 @@ class ImageLabelerBase(QMainWindow):
         self.setStyleSheet('QPushButton:!hover {color: red}')
         self.tools = self.addToolBar('Tools')
         self.tool_items = setup_toolbar(self)
-        self.left_widgets = {'Image': self.current_image_area()}
+        self.left_widgets = {'Image': self.current_image_area('', self)}
         self.right_widgets = {'Label Title': QLabel('Label List'), 'Label List': QListWidget(),
                               'Photo Title': QLabel('Photo List'), 'Photo List': QListWidget()}
         self.setStatusBar(QStatusBar(self))
@@ -108,7 +125,6 @@ class ImageLabelerBase(QMainWindow):
 
     def adjust_widgets(self):
         self.left_layout.addWidget(self.left_widgets['Image'])
-        self.left_widgets['Image'].setPixmap(QPixmap(self.current_image))
         for widget in self.right_widgets.values():
             self.right_layout.addWidget(widget)
         self.right_widgets['Photo List'].clicked.connect(self.display_selection)
@@ -120,20 +136,8 @@ class ImageLabelerBase(QMainWindow):
             return [path for path in self.image_paths.values()][current_selection]
 
     def display_selection(self):
-        image_path = self.get_current_selection()
-        self.set_current_display(image_path)
-        if image_path:
-            self.right_widgets['Label Title'].setText(f'Label List of: {image_path.split("/")[-1]}')
-
-    def set_current_display(self, image_path):
-        try:
-            uploaded = cv2.imread(image_path)
-            resized = cv2.resize(uploaded, self.image_display_size)
-            height, width = self.image_display_size
-            self.current_image = QImage(resized, height, width, QImage.Format_RGB888)
-            self.left_widgets['Image'].setPixmap(QPixmap(self.current_image))
-        except (cv2.error, FileNotFoundError) as e:
-            print(f'Image {image_path} could not be loaded: {e}')
+        self.current_image = self.get_current_selection()
+        self.left_widgets['Image'].switch_image(self.current_image)
 
     def upload_photos(self):
         file_dialog = QFileDialog()
@@ -158,16 +162,16 @@ class ImageLabelerBase(QMainWindow):
 
     def switch_editor(self, image_area):
         self.left_layout.removeWidget(self.left_widgets['Image'])
-        self.left_widgets['Image'] = image_area(self)
+        self.left_widgets['Image'] = image_area(self.current_image, self)
         self.left_layout.addWidget(self.left_widgets['Image'])
 
     def edit_mode(self):
         if self.windowTitle() == 'Image Labeler':
             self.setWindowTitle('Image Labeler(Editor Mode)')
-            self.switch_editor(ImageArea)
+            self.switch_editor(ImageEditorArea)
         else:
             self.setWindowTitle('Image Labeler')
-            self.switch_editor(QLabel)
+            self.switch_editor(RegularImageArea)
         self.display_selection()
 
     def save_changes(self):
--- test.py
+++ test.py
@@ -1,25 +1,63 @@
+from PyQt5.QtWidgets import *
 import sys
-from PyQt5.QtWidgets import (QApplication, QLabel, QMainWindow)
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
 
 
-class MouseTracker(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.initUI()
-        self.label = QLabel(self)
-        self.setMouseTracking(True)
+class Label(QLabel):
+    def __init__(self, img):
+        super(Label, self).__init__()
+        self.setFrameStyle(QFrame.StyledPanel)
+        self.pixmap = QPixmap(img)
+        self.start_point = QPoint()
+        self.end_point = QPoint()
+        self.begin = QPoint()
+        self.end = QPoint()
 
-    def initUI(self):
-        self.setGeometry(300, 300, 300, 200)
-        self.setWindowTitle('Mouse Tracker')
-        self.show()
+    def paintEvent(self, event):
+        super().paintEvent(event)
+        qp = QPainter(self)
+        size = self.size()
+        point = QPoint(0,0)
+        scaledPix = self.pixmap.scaled(size, Qt.KeepAspectRatio, Qt.SmoothTransformation)
+        point.setX((size.width() - scaledPix.width())/2)
+        point.setY((size.height() - scaledPix.height())/2)
+        qp.drawPixmap(point, scaledPix)
+        pen = QPen(Qt.red)
+        qp.setPen(pen)
+        qp.drawRect(QRect(self.begin, self.end))
+
+    def mousePressEvent(self, event):
+        self.start_point = event.pos()
+        self.begin = event.pos()
+        self.end = event.pos()
+        self.update()
 
     def mouseMoveEvent(self, event):
-        self.statusBar().showMessage('Mouse coords: ( %d : %d )' % (event.x(), event.y()))
+        self.end = event.pos()
+        self.update()
+
+    def mouseReleaseEvent(self, event):
+        self.begin = event.pos()
+        self.end = event.pos()
+        self.end_point = event.pos()
+        self.update()
 
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = MouseTracker()
-    sys.exit(app.exec_())
+class Main(QWidget):
+    def __init__(self):
+        super(Main, self).__init__()
+        layout = QH
+        label = Label(r'test.jpg')
+        layout.addWidget(label)
+        layout.setRowStretch(0,1)
+        layout.setColumnStretch(0,1)
 
+        self.setLayout(layout)
+        self.show()
+
+
+if __name__ =="__main__":
+    test = QApplication(sys.argv)
+    test_window = Main()
+    sys.exit(test.exec_())
