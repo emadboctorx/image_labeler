25e46db51dcc431384514a377e53adc27b32f8c1
Emad <emad_1989@hotmail.com>
2020-03-03 10:58:06 -0800

    added requirements and ad preview image

---
+++ labelpix/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='labelpix',
+    version='1.0.0',
+    packages=[''],
+    package_dir={'': 'Object Detection Utils/labelpix'},
+    url='https://github.com/emadboctorx/labelpix',
+    license='MIT',
+    author='emadboctor',
+    author_email='eboctor0@gmail.com',
+    description='Labeling tool for object detection', install_requires=['PyQt5']
+)
